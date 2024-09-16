dd8c55de788ac9fed5ca0b26d84f8560501a0929
ShahinAnsari25 <149264707+ShahinAnsari25@users.noreply.github.com>
2024-09-16 22:33:57 +0530

    updated

--- Routes/CreateUser.js
+++ Routes/CreateUser.js
@@ -1,61 +1,62 @@
-const express= require('express');
-const router=express.Router();
-const User= require('../models/User.js')
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User.js')
 const { body, validationResult } = require('express-validator');
-const bcrypt= require('bcryptjs')
-const jwt= require('jsonwebtoken')
-const jwtSecret='shdfhsgfhdhfjhjd'
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const jwtSecret = 'shdfhsgfhdhfjhjd'
 router.post("/createUser",
-   [body('email','invalid email').isEmail()]
-   ,async(req, res)=>{
+   [body('email', 'invalid email').isEmail()]
+   , async (req, res) => {
       const result = validationResult(req);
       if (result.isEmpty()) {
-         const salt= await bcrypt.genSalt(10);
-         const securePassword= await bcrypt.hash(req.body.password, salt)
-         try{
+         const salt = await bcrypt.genSalt(10);
+         const securePassword = await bcrypt.hash(req.body.password, salt)
+         try {
             await User.create({
-               name:req.body.name,
-               address:req.body.address,
-               email:req.body.email,
-               password:securePassword
+               name: req.body.name,
+               address: req.body.address,
+               email: req.body.email,
+               password: securePassword
             });
-           return res.status(200).json({message:'registered successfully'})
+            console.log("registered in new db")
+            return res.status(200).json({ message: 'registered successfully' })
          }
-         catch(error){
-           return res.json({message:error})
+         catch (error) {
+            return res.json({ message: error })
          }
       }
-    
+
       res.status(400).json({ errors: result.array() });
-      
-})
+
+   })
 
 //login
-router.post("/login",async(req, res)=>{
-         try{
-            const userEmail= req.body.email;
-            
-            const userData= await User.findOne({email:userEmail})
-            
-            if(!userData){
-               return res.status(404).json({message:"No user found"})
+router.post("/login", async (req, res) => {
+   try {
+      const userEmail = req.body.email;
+
+      const userData = await User.findOne({ email: userEmail })
+
+      if (!userData) {
+         return res.status(404).json({ message: "No user found" })
+      }
+      //comparing entered password with user password
+      const passwordCompare = await bcrypt.compare(req.body.password, userData.password)
+      if (passwordCompare) {
+         const data = {
+            user: {
+               id: userData.id
             }
-            //comparing entered password with user password
-            const passwordCompare= await bcrypt.compare(req.body.password,userData.password)
-            if(passwordCompare){
-               const data={
-                  user:{
-                     id:userData.id
-                  }
-               }
-               const authToken= jwt.sign(data, jwtSecret)
-               return res.status(200).json({message:'login successful', authToken:authToken, userId:userData._id, name: userData.name})
-            }
-            return res.status(404).json({message:'Invalid credentials'})
          }
-         catch(error){
-            res.status(500).json({message:error})
-         }
-      
+         const authToken = jwt.sign(data, jwtSecret)
+         return res.status(200).json({ message: 'login successful', authToken: authToken, userId: userData._id, name: userData.name })
+      }
+      return res.status(404).json({ message: 'Invalid credentials' })
+   }
+   catch (error) {
+      res.status(500).json({ message: error })
+   }
+
 })
-module.exports= router;
+module.exports = router;
--- db.js
+++ db.js
@@ -1,22 +1,23 @@
 const mongoose = require('mongoose');
-const connectionUrl="mongodb://localhost:27017/FoodAppMern"
-const mongoDB= async()=>{
-   try{
+const connectionUrl = "mongodb+srv://shahinansari107:W2i0uYSQQEasKTO2@cluster0.ee6wo.mongodb.net/FoodEase_App?retryWrites=true&w=majority&appName=Cluster0"
+const mongoDB = async () => {
+   try {
       await mongoose.connect(connectionUrl)
       console.log("connected");
-      const db= mongoose.connection.db;
-      const fetched_data= db.collection("FoodItems");
-      const data= await fetched_data.find({}).toArray();
-      global.FoodItems=data;
+      const db = mongoose.connection.db;
+      const fetched_data = db.collection("FoodItems");
+      const data = await fetched_data.find({}).toArray();
+      global.FoodItems = data;
 
-      const fetched_data_category= db.collection("Category");
-      const dataCategory= await fetched_data_category.find({}).toArray();
-      global.FoodCategory=dataCategory;
-      // console.log(data);
+      const fetched_data_category = db.collection("Category");
+      const dataCategory = await fetched_data_category.find({}).toArray();
+      global.FoodCategory = dataCategory;
+
+      console.log(data);
    }
-   catch(error){
+   catch (error) {
       console.log(error);
-      
+
    }
 }
-module.exports= mongoDB;
+module.exports = mongoDB;
